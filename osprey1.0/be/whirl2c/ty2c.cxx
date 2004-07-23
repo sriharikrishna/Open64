37bc096ee357e7a2860b062d735fcf802052d987
fzhao <fzhao@856677d4-e204-44a3-822c-5496da239ec1>
2004-07-23 17:40:09 +0000

    get rid of an assertion which use some funcion doesn't 	 appear  in whirl2c.so

--- osprey1.0/be/whirl2c/ty2c.cxx
+++ osprey1.0/be/whirl2c/ty2c.cxx
@@ -37,8 +37,8 @@
  * ====================================================================
  *
  * Module: ty2c.c
- * $Revision: 1.8 $
- * $Date: 2004-06-30 20:28:17 $
+ * $Revision: 1.9 $
+ * $Date: 2004-07-23 17:40:09 $
  * $Author: fzhao $
  * $Source: /m_home/m_utkej/Argonne/cvs2svn/cvs/Open64/osprey1.0/be/whirl2c/ty2c.cxx,v $
  *
@@ -54,7 +54,7 @@
  * ====================================================================
  */
 #ifdef _KEEP_RCS_ID
-static char *rcs_id = "$Source: /m_home/m_utkej/Argonne/cvs2svn/cvs/Open64/osprey1.0/be/whirl2c/ty2c.cxx,v $ $Revision: 1.8 $";
+static char *rcs_id = "$Source: /m_home/m_utkej/Argonne/cvs2svn/cvs/Open64/osprey1.0/be/whirl2c/ty2c.cxx,v $ $Revision: 1.9 $";
 #endif /* _KEEP_RCS_ID */
 
 #include "whirl2c_common.h"
@@ -762,12 +762,13 @@ TY2C_Translate_Structure(TY_IDX ty,CONTEXT context)
    FLD_ITER     fld_iter;
    const UINT   current_indent = Current_Indentation();
    TY& ty_rt  = Ty_Table[ty];
-   
+
+/*
    ASSERT_DBG_FATAL(TY_kind(ty_rt) == KIND_STRUCT, 
 		    (DIAG_W2F_UNEXPECTED_TYPE_KIND, 
 		     TY_kind(ty_rt), "TY2C_Translate_Structure"));
-
    struct_tokens = New_Token_Buffer();
+*/
 
    /* header of the structure */
    if (TY_is_union(ty))
