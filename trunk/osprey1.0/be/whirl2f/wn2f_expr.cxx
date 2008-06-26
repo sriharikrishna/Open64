fc539542243b9163014ee5e0be178a9cbe78940d
ju1 <ju1@856677d4-e204-44a3-822c-5496da239ec1>
2008-06-26 15:47:43 +0000

    this change addresses the translation of .eqv. vs .eq. in a case like IF((FINALDATE .GT. 0) .eqv.(INITIALDATE .GT. 0)) Here the children of the .eqv. which is translated as OPC_I4I4EQ are boolean operators but since they are not CALLs nor do they have the 1ty property the kid type was not set and the subsequent check fails to recognize the boolean context.

--- trunk/osprey1.0/be/whirl2f/wn2f_expr.cxx
+++ trunk/osprey1.0/be/whirl2f/wn2f_expr.cxx
@@ -685,12 +685,12 @@ WN2F_Infix_Op(TOKEN_BUFFER tokens,
 	 if (WN_operator(wn0) == OPR_CALL) {
 	    kid0_ty = TY_ret_type(ST_pu_type(WN_st(wn0)));
 	 } 
-	 else if (OPERATOR_has_1ty(WN_operator(wn0))) {
+	 else if (OPERATOR_has_1ty(WN_operator(wn0)) || OPERATOR_is_boolean(WN_operator(wn0))) {
 	    kid0_ty = WN_ty(wn0);
 	 }
 	 if (WN_operator(wn1) == OPR_CALL) {
 	    kid1_ty = TY_ret_type(ST_pu_type(WN_st(wn1)));
-	 } else if (OPERATOR_has_1ty(WN_operator(wn1))) {
+	 } else if (OPERATOR_has_1ty(WN_operator(wn1)) || OPERATOR_is_boolean(WN_operator(wn0))) {
 	    kid1_ty = WN_ty(wn1);
 	 }
       }
