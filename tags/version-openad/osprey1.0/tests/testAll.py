40a3f82e206e1664f85f93b48f9a325de0267e80
ju1 <ju1@856677d4-e204-44a3-822c-5496da239ec1>
2008-10-31 15:08:12 +0000

    update

--- tags/version-openad/osprey1.0/tests/testAll.py
+++ tags/version-openad/osprey1.0/tests/testAll.py
@@ -7,6 +7,90 @@ import re
 
 sepLength=80
 makeCmd="make"
+globalBatchMode=False
+globalIgnoreFailingCases=False
+globalOfferAcceptAsDefault=False
+globalAcceptAll=False
+globalVerbose=False
+
+class MultiColumnOutput:
+
+    ourColumnSeparator=2
+    
+    @staticmethod
+    def __getTermWidth():
+        import termios, fcntl, struct, sys
+        s = struct.pack("HHHH", 0, 0, 0, 0)
+        fd_stdout = sys.stdout.fileno()
+        cols=80
+	try:
+          x = fcntl.ioctl(fd_stdout, termios.TIOCGWINSZ, s)
+          (rows,cols,xPix,yPix)=struct.unpack("HHHH", x)
+	except IOError:
+	  if globalVerbose:
+	    sys.stderr.write("Error: cannot detect terminal width\n")
+	    sys.stderr.flush()
+        return cols
+
+    class ColumnInfo:
+
+        ourMinimalWidth=2
+        
+        def __init__(self):
+            self.width=self.ourMinimalWidth # minimal width
+
+    def __init__(self,stringList) :
+        self.stringList=stringList
+        self.totalWidth=self.__getTermWidth()
+
+    def __computeColumnInfos(self):
+        goodColumnInfos=[]
+        maxCols=self.totalWidth/self.ColumnInfo.ourMinimalWidth
+        listLen=len(self.stringList)
+        if listLen==0:
+            return goodColumnInfos
+        numCols=1 # 1 column to start with
+        while (True) :
+            if numCols>listLen:
+                break
+            columnInfos=[]
+            colLength=listLen/numCols
+            if (listLen%numCols>0):
+                colLength+=1
+            for i in xrange(0,numCols):
+                columnInfos.append(self.ColumnInfo())
+            for i in xrange(0,listLen):
+                # determine the column:
+                colForThis=i/colLength
+                if columnInfos[colForThis].width<len(self.stringList[i])+self.ourColumnSeparator :
+                    columnInfos[colForThis].width=len(self.stringList[i])+self.ourColumnSeparator
+            totalColumnWidth=sum([ c.width for c in columnInfos ])
+            if totalColumnWidth>self.totalWidth and numCols>1: 
+              break
+            goodColumnInfos=columnInfos[:]
+            numCols+=1
+        return goodColumnInfos
+    
+    def printIt(self):
+        columnInfos=self.__computeColumnInfos()
+        listLen=len(self.stringList)
+        numCols=len(columnInfos)
+        numRows=listLen/numCols
+        if (listLen%numCols>0) :
+            numRows+=1
+        thisRow=0
+        while thisRow<numRows:
+            listPos=thisRow
+            for columnInfo in columnInfos:
+                if listPos>=listLen:
+                    break
+                outString=self.stringList[listPos]
+                outString+=(' ' * (columnInfo.width - len(self.stringList[listPos])))
+                sys.stdout.write(outString)
+                listPos+=numRows
+            sys.stdout.write('\n')
+            thisRow+=1
+        sys.stdout.flush()
 
 class NumericalError(Exception):
     """Exception thrown when the numerical comparison discovers error that is beyond the given threshold"""
@@ -24,9 +108,22 @@ def fileCompare(fcfileName,fcmode,ignoreString):
     referenceFile = "Reference/"+fcmode + fcfileName
     if not (os.path.exists(referenceFile)):
 	sys.stdout.write(referenceFile +" not available")
-	if not (os.environ.has_key('BATCHMODE')):
-	    if (raw_input(", copy and svn add it? (y)/n: ") == "n"):
+	if not (globalBatchMode):
+            answer=""
+            if globalAcceptAll:
+                answer="y"
+            else:    
+                if (globalOfferAcceptAsDefault) :
+                    answer = (raw_input(", copy and svn add it? (y)/n: "))
+                    if (answer != "n") :
+                        answer="y"
+                else:
+                    answer = (raw_input(", copy and svn add it? y/(n): "))
+                    if (answer != "y") :
+                        answer="n"
+            if (answer == "n"):
 		sys.stdout.write("cannot verify %s\n" % fcfileName)
+                sys.stdout.flush()
 		return 0
 	    else:
 		shutil.copy(fcfileName,referenceFile)
@@ -34,14 +131,28 @@ def fileCompare(fcfileName,fcmode,ignoreString):
 		    raise RuntimeError, "\"svn add "+referenceFile+" not successful" 
 	else: # BATCHMODE
 	    sys.stdout.write("\n")
+            sys.stdout.flush()
+            return 0
     cmd="diff -I '"+ignoreString+"' "+fcfileName+" "+referenceFile        
     hasDiff = os.system(cmd)
     if (hasDiff == 512):
 	raise RuntimeError, "command "+cmd+" not successful"
     elif (hasDiff == 256):
 	sys.stdout.write("Transformation -- diff "+fcfileName+" "+referenceFile+"\n")
-	if not (os.environ.has_key('BATCHMODE')):
-	    if (raw_input("accept/copy new %s to %s? (y)/n: " % (fcfileName,referenceFile)) == "n"):
+	if not (globalBatchMode):
+            answer=""
+            if globalAcceptAll:
+                answer="y"
+            else:    
+                if (globalOfferAcceptAsDefault) :
+                    answer = raw_input("accept/copy new %s to %s? (y)/n: " % (fcfileName,referenceFile))
+                    if (answer != "n"):
+                        answer="y"
+                else:
+                    answer = raw_input("accept/copy new %s to %s? y/(n): " % (fcfileName,referenceFile))
+                    if (answer != "y"):
+                        answer="n"
+            if (answer == "n"):
 		sys.stdout.write("skipping change\n")
 	    else:
 		shutil.copy(fcfileName,referenceFile)
@@ -58,6 +169,14 @@ def printSep(sepChar,msg,sepLength):
     sys.stdout.write("\n")
     sys.stdout.flush()
 
+def numberedList(list):
+    numberedList=[]
+    digits=len(str(len(list)+1))
+    format='%'+str(digits)+'d:%s'
+    for (c,e) in enumerate(list):
+        numberedName=format%(c+1,e)
+        numberedList.append(numberedName)
+    return numberedList
 
 def populateExamplesList(args):
     import glob
@@ -74,7 +193,7 @@ def populateExamplesList(args):
     examples = []
     exampleRegexs = []
     if (len(args) == 0): # no arguments
-	if (os.environ.has_key('BATCHMODE')):
+	if (globalBatchMode):
 	    examples = allExamples
 	else:
 	    done = False
@@ -82,7 +201,8 @@ def populateExamplesList(args):
 		done = True
 		sys.stdout.write("pick from the following examples:\n")
 		sys.stdout.flush()
-		print allExamples
+                examples = allExamples
+                MultiColumnOutput(numberedList(examples)).printIt()    
 		examplesInput = raw_input("enter one or more regular expressions here or '(all [%i | %i %i])': ").split()
 		if (len(examplesInput) == 0):			# no arguments
 		    examples = allExamples
@@ -136,21 +256,12 @@ def populateExamplesList(args):
 	rangeEnd = len(examples)
 	printSep("=","",sepLength)
 	sys.stdout.write("The following examples match the given regular expression(s):\n")
-	i = 0
-	for ex in examples:
-	    i = i + 1
-	    sys.stdout.write("[%i-%s]  " % (i,ex))
-	sys.stdout.write("\n")
+        MultiColumnOutput(numberedList(examples)).printIt()    
 	printSep("=","",sepLength)
     else:
 	printSep("=","",sepLength)
 	sys.stdout.write("Running all examples with a range of (%i-%i):\n" % (rangeStart,rangeEnd))
-	i = 0
-        for ex in examples:
-            i = i + 1
-	    if (i >= rangeStart and i <= rangeEnd):
-		sys.stdout.write("[%i-%s]  " % (i,ex))
-	sys.stdout.write("\n")
+        MultiColumnOutput(numberedList(examples)[rangeStart-1:rangeEnd]).printIt()    
 	printSep("=","",sepLength)
     return (examples,rangeStart,rangeEnd)
 
@@ -161,12 +272,43 @@ def runTest(exName,exNum,totalNum):
     whirl2f=os.path.join(os.environ['OPEN64ROOT'],'whirl2f','whirl2f')
     ir_b2a=os.path.join(os.environ['OPEN64ROOT'],'ir_tools','ir_b2a')
     sys.stdout.flush()
-    printSep("*","** testing %i of %i (%s)" % (exNum,totalNum,exName),sepLength)
+    basename,ext=os.path.splitext(exName)
+    haveRef=os.path.exists(os.path.join('Reference',basename+'.w2f.f'))
+    if not haveRef :
+        if globalIgnoreFailingCases : 
+            printSep("*","** skipping %i of %i (%s) - no reference file" % (exNum,totalNum,exName),sepLength)
+            return 0
+        else: 
+            printSep("*","** testing %i of %i (%s)" % (exNum,totalNum,exName),sepLength)
+	sys.stdout.write("No reference file available")
+	if not (globalBatchMode):
+            answer=""
+            if globalAcceptAll:
+                answer="y"
+            else:    
+                if (globalOfferAcceptAsDefault) :
+                    answer = (raw_input(", run it anyway? (y)/n: "))
+                    if (answer != "n") :
+                        answer="y"
+                else:
+                    answer = (raw_input(", run it anyway? y/(n): "))
+                    if (answer != "y") :
+                        answer="n"
+            if (answer == "n"):
+		sys.stdout.write("\n")
+                sys.stdout.flush()
+		return 0
+	else:
+            if ( not globalAcceptAll): 
+                sys.stdout.write("\n")
+                sys.stdout.flush()
+                return 0
+    else:
+        printSep("*","** testing %i of %i (%s)" % (exNum,totalNum,exName),sepLength)
     # simple mfef90
     cmd=mfef90 + " " + exName
     if (os.system(cmd)):
 	raise MakeError, "Error while executing \"" + cmd + "\""
-    basename,ext=os.path.splitext(exName)
     cmd=whirl2f + " " + basename+".B > /dev/null 2>&1"
     if (os.system(cmd)):
 	raise MakeError, "Error while executing \"" + cmd + "\""
@@ -211,21 +353,69 @@ def runTest(exName,exNum,totalNum):
 
 
 def main():
+    from optparse import OptionParser
+    usage = '%prog [options] '
+    compilers=['ifort','gfortran','g95','f95','openf95']
+    compilerOpts='[ '
+    for i in compilers :
+        compilerOpts+=i
+        if  i != compilers[-1]:
+            compilerOpts+=" | "
+    compilerOpts+=" ]"        
+    opt = OptionParser(usage=usage)
+    opt.add_option('-i','--ignoreFailingCases',dest='ignoreFailingCases',
+                   help="don't if we should try to run  the cases known to fail",
+                   action='store_true',default=False)
+    opt.add_option('-a','--offerAcceptAsDefault',dest='offerAcceptAsDefault',
+                   help="offer accept as default for updating reference files",
+                   action='store_true',default=False)
+    opt.add_option('-A','--acceptAll',dest='acceptAll',
+                   help="accept all changes without confirmation",
+                   action='store_true',default=False)
+    opt.add_option('-b','--batchMode',dest='batchMode',
+                   help="run in batchMode suppressing output",
+                   action='store_true',default=False)
+    opt.add_option('-v','--verbose',dest='verbose',
+                   help="let the pipeline components produce some extra output",
+                   action='store_true',default=False)
+    opt.add_option('-c','--compiler',dest='compiler',
+                   type='choice', choices=compilers,
+                   help="pick a compiler (defaults to ifort) from the following list: " +compilerOpts+" - the compiler should be in PATH",
+                   default='ifort')
+    opt.add_option('-O','--optimize',dest='optimize',
+                   help="turn compiler optimization on (default off)",
+                   action='store_true',default=False)
+    (options, args) = opt.parse_args()
     try:
+        if os.environ.has_key('BATCHMODE') or options.batchMode :
+            global globalBatchMode
+            globalBatchMode=True
+        if options.ignoreFailingCases :
+            global globalIgnoreFailingCases
+            globalIgnoreFailingCases=True
+        if options.offerAcceptAsDefault :
+            global globalOfferAcceptAsDefault
+            globalOfferAcceptAsDefault=True
+        if options.acceptAll :
+            global globalAcceptAll
+            globalAcceptAll=True
+        if options.verbose :
+            global globalVerbose
+            globalVerbose=True
+        if options.compiler :
+            os.environ['F90C']=options.compiler
+        if options.optimize :
+            os.environ['OPTIMIZE']='true'
 	if not (os.environ.has_key('OPEN64ROOT')):
 	    raise ConfigError, "environment variable OPEN64ROOT not defined"
-
 	if not (os.environ.has_key('F90C')):
             os.environ['F90C']='ifort'
-
 	if not (os.environ.has_key('F90FLAGS')):
             os.environ['F90FLAGS']=''
-
         cmd=os.environ['F90C']+" "+os.environ['F90FLAGS']+" -c "+ "Extras/w2f__types.f90"
         if (os.system(cmd)):
             raise MakeError, "Error while executing \"" + cmd + "\""
-
-	(examples,rangeStart,rangeEnd) = populateExamplesList(sys.argv[1:])
+	(examples,rangeStart,rangeEnd) = populateExamplesList(args[0:])
 	# Run the examples
 	j = rangeStart-1
 	while (j < rangeEnd):
@@ -233,26 +423,26 @@ def main():
 		runTest(examples[j],j+1,len(examples))
 	    except ConfigError, errtxt:
 		print "ERROR (environment configuration) in test %i of %i (%s): %s" % (j+1,len(examples),examples[j],errtxt)
-		if not (os.environ.has_key('BATCHMODE')):
+		if not (globalBatchMode):
 		    if (raw_input("Do you want to continue? (y)/n: ") == "n"):
 			return -1
 		else:
 		    return -1
 	    except MakeError, errtxt:
 		print "ERROR in test %i of %i (%s) while executing \"%s\"." % (j+1,len(examples),examples[j],errtxt)
-		if not (os.environ.has_key('BATCHMODE')):
+		if not (globalBatchMode):
 		    if (raw_input("Do you want to continue? (y)/n: ") == "n"):
 			return -1
 		else:
 		    return -1
 	    except NumericalError:
 		print "NUMERICAL ERROR in test %i of %i (%s)." % (j+1,len(examples),examples[j])
-		if not (os.environ.has_key('BATCHMODE')):
+		if not (globalBatchMode):
 		    if (raw_input("Do you want to continue? (y)/n: ") == "n"):
 			return -1
 	    except RuntimeError, errtxt:
 		print "ERROR in test %i of %i (%s): %s." % (j+1,len(examples),examples[j],errtxt)
-		if not (os.environ.has_key('BATCHMODE')):
+		if not (globalBatchMode):
 		    if (raw_input("Do you want to continue? (y)/n: ") == "n"):
 			return -1
 		else:
