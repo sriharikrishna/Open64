d99f1549389ece67431ecde7e9c48d453346f021
ju1 <ju1@856677d4-e204-44a3-822c-5496da239ec1>
2009-11-02 16:02:15 +0000

    changes reflecting rev 864

--- osprey1.0/tests/Reference/equivalence4.w2f.f
+++ osprey1.0/tests/Reference/equivalence4.w2f.f
@@ -1,5 +1,5 @@
 C ***********************************************************
-C Fortran file translated from WHIRL Fri Oct 30 09:41:17 2009
+C Fortran file translated from WHIRL Fri Oct 30 17:26:31 2009
 C ***********************************************************
 C ***********************************************************
 
@@ -10,21 +10,16 @@ C
 C     **** Local Variables and Functions ****
 C
       
-      EQUIVALENCE(tmp0(1), F1)
-      EQUIVALENCE(tmp0(1), V1)
+      EQUIVALENCE(V1, F1)
+      
       REAL(w2f__4) F1
       
-      EQUIVALENCE(tmp1(1), F2)
-      EQUIVALENCE(tmp1(1), V2)
+      EQUIVALENCE(V2, F2)
+      
       REAL(w2f__4) F2
       REAL(w2f__4) V1
       REAL(w2f__4) V2
       REAL(w2f__4) X
-C
-C     **** Temporary Variables ****
-C
-      INTEGER(w2f__i1) tmp0(1 : 4)
-      INTEGER(w2f__i1) tmp1(1 : 4)
 C
 C     **** Statements ****
 C
