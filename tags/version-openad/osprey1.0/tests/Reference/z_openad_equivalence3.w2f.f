9fe08277503c5ca0094f1aa9f4850a51b155e534
ju1 <ju1@856677d4-e204-44a3-822c-5496da239ec1>
2009-11-02 16:03:33 +0000

    more changes reflecting rev 864

--- tags/version-openad/osprey1.0/tests/Reference/z_openad_equivalence3.w2f.f
+++ tags/version-openad/osprey1.0/tests/Reference/z_openad_equivalence3.w2f.f
@@ -6,20 +6,15 @@ C
 C     **** Local Variables and Functions ****
 C
       
-      EQUIVALENCE(tmp0(1), F1)
-      EQUIVALENCE(tmp0(1), V1)
+      EQUIVALENCE(V1, F1)
+      
       REAL(w2f__4) F1
       
-      EQUIVALENCE(tmp1(1), F2)
-      EQUIVALENCE(tmp1(1), V2)
+      EQUIVALENCE(V2, F2)
+      
       REAL(w2f__4) F2
       REAL(w2f__4) V1
       REAL(w2f__4) V2
-C
-C     **** Temporary Variables ****
-C
-      INTEGER(w2f__i1) tmp0(1 : 4)
-      INTEGER(w2f__i1) tmp1(1 : 4)
 C
 C     **** Statements ****
 C
